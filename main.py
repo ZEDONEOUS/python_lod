a8d15648f6bbe9dc1b4a40790984770fa1761646
ZEDONEOUS <santiago.silcart@gmail.com>
2017-01-22 16:37:04 -0500

    adicion de filtro de la metadata traida por dataset

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def site_read(query_point = "https://datahub.io/"):
         with open("site_read.json") as data_file:
             data = json.load(data_file)
             flag = data["success"]
+        data_file.close()
     except Exception as e:
         print "EXCEPCION EN LA CONEXION CON EL SITIO - " + str(e)
 
@@ -35,7 +36,7 @@ def fetch_datasets_package_list(limit = 0, query_point = "https://datahub.io/"):
     if(site_read(query_point)):
         consulta = "http --json --pretty format "
         consulta = consulta + query_point + "api/3/action/package_list"
-        consulta = consulta + " limit=" + str(limit) + " > output.json"
+        consulta = consulta + " limit=" + str(limit) + " > datasets_package_list.json"
         os.system(consulta);
     else:
         print "ERROR - IMPOSIBLE CONECTAR CON EL SITIO ESPECIFICADO"
@@ -54,5 +55,62 @@ def fetch_dataset_package_metadata(id_package, query_point):
     consulta = consulta + "?id=" + str(id_package) + " > package_metadata.json"
     os.system(consulta);
 
-#fetch_dataset_package_metadata("0000-0003-4469-8298", "https://datahub.io/")
-fetch_datasets_package_list()
+"""
+    Obtiene una lista de revision de un dataset en especifico
+    Variables:
+        id_package = id o nombre del dataset(paquete) del cual traer la lista de revision
+        query_point = punto de consulta sitio web
+"""
+def fetch_package_revision_list(id_package, query_point):
+    consulta = "http --json --pretty format "
+    consulta = consulta + query_point + "api/3/action/package_revision_list"
+    consulta = consulta + "?id=" + str(id_package) + " > package_revision_list.json"
+    os.system(consulta);
+
+"""
+    Obtiene el metadata filtrado de cada dataset consultado previamente de la lista
+    Datos filtrados:
+        title = nombre del dataset
+        revision_id = id con el cual se puede consultar la lista de revision
+        author = creador del dataset
+        license_title = licencia bajo la cual se encuentra el recurso
+        metadata_created = fecha de la creacion del metadata del dataset
+        metadata_modified = fecha de la ultima modificacion del metadata del dataset
+"""
+def fetch_all_datasets_package_metadata():
+    fetch_datasets_package_list()
+    with open("datasets_package_list.json") as data_file_list:
+        datasets_list = json.load(data_file_list)
+        metadata = {}
+        metadata["packages"] = []
+        for i in xrange(0, 10):
+            fetch_dataset_package_metadata(datasets_list["result"][i], "https://datahub.io/")
+            with open("package_metadata.json") as metadata_file:
+                package_metadata = json.load(metadata_file)
+                temp_obj = {}
+                temp_obj["title"] = package_metadata["result"]["title"]
+                temp_obj["revision_id"] = package_metadata["result"]["revision_id"]
+                temp_obj["author"] = package_metadata["result"]["author"]
+                temp_obj["license_title"] = package_metadata["result"]["license_title"]
+                temp_obj["metadata_created"] = package_metadata["result"]["metadata_created"]
+                temp_obj["metadata_modified"] = package_metadata["result"]["metadata_modified"]
+
+                temp_obj["resources"] = []
+                for j in xrange(0, len(package_metadata["result"]["resources"])):
+                    temp_obj_resources = {}
+                    temp_obj_resources["name"] = package_metadata["result"]["resources"][j]["name"]
+                    temp_obj_resources["last_modified"] = package_metadata["result"]["resources"][j]["last_modified"]
+                    temp_obj_resources["state"] = package_metadata["result"]["resources"][j]["state"]
+                    temp_obj_resources["url"] = package_metadata["result"]["resources"][j]["url"]
+                    temp_obj_resources["format"] = package_metadata["result"]["resources"][j]["format"]
+                    temp_obj["resources"].append(temp_obj_resources)
+
+                metadata["packages"].append(temp_obj)
+            metadata_file.close()
+    data_file_list.close()
+    with open('package_lecture_filter.json', 'w') as outfile:
+        json.dump(metadata, outfile, indent=4, sort_keys=True)
+    outfile.close()
+
+
+fetch_all_datasets_package_metadata()
